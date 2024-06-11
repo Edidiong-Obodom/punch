3117254a591c23f2129905c4113000916c805779
Edidiong Obodom <97753291+DinnoCreator@users.noreply.github.com>
2024-06-11 11:34:27 +0100

    feat: added in good hands component

---
+++ src/components/in_good_hands/index.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+const InGoodHands = () => {
+  return (
+    <div className="mt-16 md:mt-24 eddyContainerFull pb-8">
+      <div className="w-full flex flex-col md:flex-row">
+        <div className="md:w-1/2">
+          <div className="">
+            <h1 className="font-bold text-[26px] md:text-[36px] max-w-[400px]">
+              How we ensure you’re in good hands.
+            </h1>
+          </div>
+          <div className="mt-5 max-w-[480px]">
+            <p>
+              With our comprehensive screening process, we hand-pick highly
+              skilled candidates so you can onboard them in a matter of days.
+            </p>
+          </div>
+        </div>
+        <div className="md:w-1/2"></div>
+      </div>
+    </div>
+  );
+};
+
+export default InGoodHands;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,6 +3,7 @@ import Navbar from "../components/nabar";
 import Hero from "../components/hero";
 import OneStop from "../components/one_stop";
 import HowItWorks from "../components/how_it_works";
+import InGoodHands from "../components/in_good_hands";
 
 const Home = () => {
   return (
@@ -11,6 +12,7 @@ const Home = () => {
       <Hero />
       <OneStop />
       <HowItWorks />
+      <InGoodHands />
     </div>
   );
 };
